c55e3919411a6058677a1138399b51eea04307b8
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-01-13 21:28:52 +0100

    Add JwtToken

--- Identity/Pages/Account/Login.cshtml.cs
+++ Identity/Pages/Account/Login.cshtml.cs
@@ -1,3 +1,4 @@
+using Identity.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -56,18 +57,5 @@ namespace Identity.Pages.Account
             return Page();
 
         }
-    }
-
-    public class Credential
-    {
-        [Required]
-        [Display(Name ="User Name")]
-        public string UserName { get; set; }
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-
-        [Display(Name = "Remember Me")]
-        public bool RememberMe { get; set; }
     }
 }
--- Identity/Pages/HRManager.cshtml.cs
+++ Identity/Pages/HRManager.cshtml.cs
@@ -1,9 +1,13 @@
+using Identity.Authorization;
 using Identity.DTO;
+using Identity.Pages.Account;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 
@@ -25,6 +29,13 @@ namespace Identity.Pages
         public async Task OnGetAsync()
         {
             var httpClient = httpClientFactory.CreateClient("OurWebAPI");
+            var res = await httpClient.PostAsJsonAsync("auth", new Credential { UserName = "admin", Password = "password" });
+            res.EnsureSuccessStatusCode();
+            string strJwt = await res.Content.ReadAsStringAsync();
+            var token = JsonConvert.DeserializeObject<JwtToken>(strJwt);
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
+
             WeatherForecastItems = await httpClient.GetFromJsonAsync<List<WeatherForecastDTO>>("WeatherForecast");
         }
     }
